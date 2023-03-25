361ae4b5e621ec394da21225f707f8354941be69
Brayan Guecha <brayanguecha01@gmail.com>
2023-03-24 21:09:02 -0500

    De añade carpeta charts

---
+++ py-project/charts/charts.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import io
+
+def generate_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [200, 34, 120]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('img.jpg'.format('svg'))
+    plt.close()
