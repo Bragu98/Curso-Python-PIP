4a0037def5413f33db2427da4b708047ce5edc8b
Brayan Guecha <brayanguecha01@gmail.com>
2023-03-24 22:22:53 -0500

    Se añade proyecto app

--- py-project/charts/charts.py
+++ py-project/charts/charts.py
@@ -9,3 +9,12 @@ def generate_pie_chart():
     ax.pie(values, labels=labels)
     plt.savefig('img.jpg'.format('svg'))
     plt.close()
+
+def generate_bar_chart():
+  labels = ['A', 'B', 'C']
+  values = [200, 34, 120]
+
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig("bar.png")
+  plt.close()
